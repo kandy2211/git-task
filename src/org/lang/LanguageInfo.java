81c2dfcc9c08e60fe9f0fcbb92d98aaac79f8ea1
kandy2211 <kanthasamy2211@gmail.com>
2022-08-19 12:45:03 +0530

    todaywork

--- src/org/lang/LanguageInfo.java
+++ src/org/lang/LanguageInfo.java
@@ -7,6 +7,10 @@ System.out.println("tamil");
 public void egnlishLanguage() {
 	System.out.println("english");
 }
+private void french() {
+	// TODO Auto-generated method stub
+
+}
 public void hindiLanguage() {
 System.out.println("hindi");
 }
